88dfb768ea343e4f27e66e442e3b58465603a91b
whn <me@whn.se>
2020-02-07 21:18:13 +0100

    fix set image

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ def main(template, set_image):
     tasks = [(overlay, template.format(overlay)) for overlay in overlays if path.isdir(overlay)]
     for (overlay, output) in tasks:
         if set_image is not None:
-            subprocess.run(['kustomize', 'edit', 'set', 'image', set_image])
+            subprocess.run(['kustomize', 'edit', 'set', 'image', set_image], cwd="./" + overlay)
         subprocess.run(["kustomize", "build", overlay, "-o", output])
 
 if __name__ == "__main__":
     if len(sys.argv) == 1:
         print("kustomize-build-all template [set-image]")
-    main(sys.argv[1], sys.argv[2] if 2 in sys.argv else None)
+    main(sys.argv[1], sys.argv[2] if len(sys.argv) > 2 else None)
